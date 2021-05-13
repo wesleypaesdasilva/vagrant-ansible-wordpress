7d3d4fd41fa515abbbf1da0f23fa8f4904846fd3
Wesley Paes Silva <wesley.paes@hepta.com.br>
2021-05-13 18:25:42 -0300

    atualizando...

--- install.sh
+++ install.sh
@@ -12,13 +12,78 @@
 
 ## Install Vagrant and Ansible
 echo "Iniciando a instalação do vagrant..."
-apt-get install vagrant -y
+echo " "
+echo " "
+echo " "
+echo " "
+#apt-get install vagrant -y
 vagrant -v
+echo " "
+echo " "
+echo " "
+echo " "
 
-sleep 5
+#sleep 5
 
 echo "Iniciando a instalação do ansible"
-apt-get install ansible -y
+#apt-get install ansible -y
+echo " "
+echo " "
+echo " "
+echo " "
+
 ansible --version
 
 sleep 5
+
+echo "Inciando o vagrant"
+echo " "
+echo " "
+echo " "
+echo " "
+
+#vagrant init
+
+sleep 5
+
+echo "Interfaces disponíveis para fazer a BRIDGE (MESMA REDE QUE A SUA MÁQUINA ESTÁ"
+echo " "
+echo " "
+
+ifconfig | grep flags | cut -d ":" -f1
+
+sleep 2
+
+echo -e "Digite o nome da interface escolhida:"
+read -p "Interface: " -t 20 INTERFACE 
+echo " "
+echo " "
+echo " "
+echo " "
+
+sleep 02
+
+echo "Você escolheu a interface $INTERFACE"
+echo " "
+echo " "
+echo " "
+echo " "
+
+sleep 2
+INTERFACE_IP=$(ifconfig wlp5s0 | grep broadcast | cut -d "t" -f2 | cut -d " " -f2)
+
+echo "Precisamos configurar 02 máquinas com os IP's na mesma rede da sua máquina que é:$INTERFACE_IP"
+
+sleep 3
+
+echo " "
+echo " "
+echo " "
+
+read -p "IP da máquina WORDPRESS: " -t 20 IP_WORDPRESS
+echo " "
+read -p "IP da máquina MYSQL: " -t 20 IP_MYSQL
+echo " "
+
+
+
